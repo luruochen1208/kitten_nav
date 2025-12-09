577edd1ca832787eba9db61d803cccc0affd2897
luruochen1208 <luruochen1208@gmail.com>
2025-12-09 13:23:51 +0800

    chore: 更新导航数据 - 2025/12/9 13:23:51

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -481,7 +481,7 @@ export const mockData = {
       ]
     }
   ],
-  "title": "猫猫导航🐱",
-  "_fileSha": "ea3f00cba99e7d9e1d5cb7b816f6a7836b8c233e",
+  "title": "思迈尔阅读馆导航",
+  "_fileSha": "061661f05b7aa741907ec428cad9a0af57da8d5a",
   "search": "baidu"
 }
